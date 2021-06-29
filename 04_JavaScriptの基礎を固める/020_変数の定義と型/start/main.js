6cd13ac947a2291ce45227bdd4a0fb517e28dcdf
RyoKitatani <ryo0929m@gmail.com>
2021-06-29 16:14:02 +0900

    finish 6-52

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,25 @@
+// var let const 
+
+//let = 上書き可能
+//const = 上書き不可
+
+// var name = 'Tom';
+
+let name = 'Tom';
+name = 'Tim';
+console.log('hello' +  name );
+
+// Number, String, Boolean, Undefined, Null, Symbol
+
+//動的型付け言語
+
+let variable  = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log(typeof variable);
+
+// 静的型付け言語 javaなど
+int num = 12;
+
