425409e9a9c8f3f371968a61b02ec3b1f707a9f4
RyoKitatani <ryo0929m@gmail.com>
2021-06-30 23:43:19 +0900

    finish 5-63

--- 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
+++ 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
@@ -15,18 +15,38 @@
 //   return "code" + name;
 // }, 'Mafia');
 
- function doSomething(a,b, callback) {
-   const result = callback(a,b);
-   console.log(result);
- }
+//  function doSomething(a,b, callback) {
+//    const result = callback(a,b);
+//    console.log(result);
+//  }
 
- function multiply(a, b) {
-   return a * b;
- }
+//  function multiply(a, b) {
+//    return a * b;
+//  }
 
- function plus(a, b) {
-  return a + b;
- }
+//  function plus(a, b) {
+//   return a + b;
+//  }
 
-doSomething(2, 6, multiply);
-doSomething(2, 6, plus);
+// doSomething(2, 6, multiply);
+// doSomething(2, 6, plus);
+
+
+const arry = [1,2,3,4,5];
+
+function forEach(arry, callback) {
+  for(let i = 0; i < arry.length; i++) {
+    callback(arry[i]);
+  }
+}
+
+function log(val) {
+  console.log(val);
+}
+
+function double(val){
+  val = val * 2;
+  log(val);
+}
+
+forEach(arry, double);
