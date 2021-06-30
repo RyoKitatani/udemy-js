bdba46b66e6b9a2c1d025cc32b6f00ebed725d69
RyoKitatani <ryo0929m@gmail.com>
2021-06-30 17:29:39 +0900

    finish 5-61

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,24 @@
+// function hello(name = "Tom") {
+//  console.log("hello" + name);
+// }
+
+//javascriptは上から順番に読まれる const
+//functionならどこでも可能
+
+// const hello = function (name = "Tom") {
+//   console.log("hello" + name);
+// }
+
+// const hello = (name, age) =>  console.log("hello" + name + age);
+
+
+// hello("code mafia", 10);
+// hello("code mafia 2", 20);
+// hello();
+
+
+const hello = (name, age) => 40;
+
+const arry = [1,2,3,4,5,6];
+
+arry.forEach(value => console.log(value));
