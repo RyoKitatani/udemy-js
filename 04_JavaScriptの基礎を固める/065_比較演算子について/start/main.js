8ed0ad19bbf4fd91644cb8562b38cbbe4f844d13
RyoKitatani <ryo0929m@gmail.com>
2021-06-30 17:19:28 +0900

    finish 5-60

---
+++ 04_JavaScriptの基礎を固める/065_比較演算子について/start/main.js
@@ -0,0 +1,20 @@
+const num = 1;
+const bool = Boolean(num);
+console.log(bool);
+
+
+// if (0 == "0" || 0 === "0" ) {
+//   console.log("this is true");
+// } else {
+//   console.log("this is false");
+// }
+
+if (num) {
+  console.log("this is true");
+} else {
+  console.log("this is false");
+}
+
+
+//&& かつ
+// || または
