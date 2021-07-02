691f7d15a8a83b8d2d4368245d8caa394b9bc903
RyoKitatani <ryo0929m@gmail.com>
2021-07-02 21:53:20 +0900

    finish 6-77

--- 04_JavaScriptの基礎を固める/112_thisを使いこなそう/start/main.js
+++ 04_JavaScriptの基礎を固める/112_thisを使いこなそう/start/main.js
@@ -1,20 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btn = document.querySelector('#btn');
     const ta = new TextAnimation('.animate-title');
-    const ta2 = new TextAnimation('.animate-title-2');
     ta.animate();
-    ta2.animate();
-    btn.addEventListener('click', function(){
-        ta.animate();
-    });
 });
 
 class TextAnimation {
     constructor(el) {
-        console.log(this)
-        this.el = document.querySelector(el);
-        this.chars = this.el.innerHTML.trim().split("");
-        this.el.innerHTML = this._splitText();
+        this.DOM = {};
+        this.DOM.el = document.querySelector(el);
+        this.chars = this.DOM.el.innerHTML.trim().split("");
+        this.DOM.el.innerHTML = this._splitText();
     }
     _splitText() {
         return this.chars.reduce((acc, curr) => {
@@ -23,6 +18,25 @@ class TextAnimation {
         }, "");
     }
     animate() {
-    this.el.classList.toggle('inview');
+    this.DOM.el.classList.toggle('inview');
+    }
+}
+
+class TweenTextAnimation extends TextAnimation {
+    constructor(el) {
+        super(el);
+        this.DOM.chars = this.DOM.el.querySelectorAll('.char')
+    }
+    animate() {
+        this.DOM.el.classList.add('inview');
+        this.DOM.chars.forEach((c, i) => {
+            TweenMax.to(c, .6, {
+                ease: Back.easeOut, 
+                delay: i * 0.05,
+                startAt: {y: '-50%', opacity: 0},
+                y: '0%',
+                opacity: 1
+            })
+        });
     }
 }
