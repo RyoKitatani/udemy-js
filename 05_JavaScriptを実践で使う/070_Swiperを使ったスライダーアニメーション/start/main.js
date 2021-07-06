539bb6e30365913dc145446b37ec303fdb7f1a02
RyoKitatani <ryo0929m@gmail.com>
2021-07-06 16:52:25 +0900

    finish 7-91

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,7 +1,9 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  effect: 'coverflow',
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider('.swiper-container');
+  hero.start({delay: 2000});
 
+  setTimeout(() => {
+    hero.stop();
+  }, 5000)
 });
+
